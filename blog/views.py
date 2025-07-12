6a1ac36b3bfdd332a0c1e4b5ed1ab5e89393d516
Yverson Jean Louis <afro999@Yversons-MBP.attlocal.net>
2025-07-12 15:04:35 -0400

    https://github.com/Yverson1/my-first-blog.git

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
+
 
 #our functions below always take in a request parameter. This request is what the frontend ask for backend
 #to serve to them
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    Posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': Posts})
